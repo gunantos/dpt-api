e7c09af935cc8e0e7ddf896fdd2a73e5dc99646f
andtho89 <gunanto.simamora@gmail.com>
2023-08-24 13:57:23 +0700

    fixed error no user router

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var { normalizePort}=require('./utils/helpers.js')
 var app = express();
 var port = normalizePort(process.env.PORT || '3000');
@@ -22,8 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
